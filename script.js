2c29e0b31e0b25ee28b83eccef5d69948ffc32b4
Younes Codes <younes.codes@gmail.com>
2021-03-03 23:59:54 +0100

    integration page star wars

---
+++ script.js
@@ -0,0 +1,36 @@
+const btnOpenMenu = document.getElementById("open-menu");
+const btnCloseMenu = document.getElementById("close-menu");
+const menu = document.getElementById("menu");
+const body = document.getElementsByTagName("body")[0];
+
+btnOpenMenu.onclick = () => {
+  menu.classList.add("show-menu");
+  body.style.overflow = "hidden";
+};
+
+btnCloseMenu.onclick = () => {
+  menu.classList.remove("show-menu");
+  body.style.overflow = "scroll";
+};
+
+const imgPub = document.getElementById("img-pub");
+const offsetTopImgPub = imgPub.offsetTop - 50;
+const offsetHeightImgPub = imgPub.offsetHeight;
+const offsetLeftImgPub = imgPub.offsetLeft;
+
+const kilokrenImg = document.getElementById("kilokren-img");
+const offsetTopKilokrenImg = kilokrenImg.offsetTop;
+const endPointImgPub = offsetTopKilokrenImg - offsetHeightImgPub - 30;
+//console.log(`img Kilokren ${offsetTopKilokrenImg}`);
+window.onscroll = () => {
+  const scrollPos = window.pageYOffset || document.documentElement.scrollTo;
+  if (scrollPos >= offsetTopImgPub) {
+    imgPub.style.cssText = `position: fixed; top: 20px; left: ${offsetLeftImgPub}px`;
+  } else {
+    imgPub.style.position = "initial";
+  }
+
+  if (scrollPos + 50 + offsetHeightImgPub >= offsetTopKilokrenImg) {
+    imgPub.style.cssText = `position: absolute; top:${endPointImgPub}px ; left: ${offsetLeftImgPub}px`;
+  }
+};
