bbf3758b1029077a9df0e256d39371155e3b1d9b
Younes Codes <younes.codes@gmail.com>
2021-03-04 00:50:39 +0100

    fix minor bug

--- script.js
+++ script.js
@@ -13,24 +13,29 @@ btnCloseMenu.onclick = () => {
   body.style.overflow = "scroll";
 };
 
-const imgPub = document.getElementById("img-pub");
-const offsetTopImgPub = imgPub.offsetTop - 50;
-const offsetHeightImgPub = imgPub.offsetHeight;
-const offsetLeftImgPub = imgPub.offsetLeft;
+window.onload = () => {
+  const imgPub = document.getElementById("img-pub");
+  const offsetTopImgPub = imgPub.offsetTop - 50;
+  const offsetHeightImgPub = imgPub.offsetHeight;
+  const offsetLeftImgPub = imgPub.offsetLeft;
 
-const kilokrenImg = document.getElementById("kilokren-img");
-const offsetTopKilokrenImg = kilokrenImg.offsetTop;
-const endPointImgPub = offsetTopKilokrenImg - offsetHeightImgPub - 30;
-//console.log(`img Kilokren ${offsetTopKilokrenImg}`);
-window.onscroll = () => {
-  const scrollPos = window.pageYOffset || document.documentElement.scrollTo;
-  if (scrollPos >= offsetTopImgPub) {
-    imgPub.style.cssText = `position: fixed; top: 20px; left: ${offsetLeftImgPub}px`;
-  } else {
-    imgPub.style.position = "initial";
-  }
-
-  if (scrollPos + 50 + offsetHeightImgPub >= offsetTopKilokrenImg) {
-    imgPub.style.cssText = `position: absolute; top:${endPointImgPub}px ; left: ${offsetLeftImgPub}px`;
-  }
+  const kilokrenImg = document.getElementById("kilokren-img");
+  const offsetTopKilokrenImg = kilokrenImg.offsetTop;
+  const endPointImgPub = offsetTopKilokrenImg - offsetHeightImgPub - 30;
+  console.log(offsetTopKilokrenImg);
+  console.log(endPointImgPub);
+  console.log(offsetHeightImgPub);
+  //console.log(`img Kilokren ${offsetTopKilokrenImg}`);
+  window.onscroll = () => {
+    const scrollPos = window.pageYOffset || document.documentElement.scrollTo;
+    if (scrollPos >= offsetTopImgPub) {
+      imgPub.style.cssText = `position: fixed; top: 20px; left: ${offsetLeftImgPub}px`;
+    } else {
+      imgPub.style.position = "initial";
+    }
+    // 50px is the height of the navbar
+    if (scrollPos + 50 + offsetHeightImgPub >= offsetTopKilokrenImg) {
+      imgPub.style.cssText = `position: absolute; top:${endPointImgPub}px ; left: ${offsetLeftImgPub}px`;
+    }
+  };
 };
